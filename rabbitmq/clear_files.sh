24f8fa04ee28e313ad0ae3744cfff5424aef6ea5
Jun-Wei Lin <jun-wei.lin@gmod-dev.nal.usda.gov>
2014-07-25 17:04:14 -0400

    update clear_file.sh and remove useless media files

--- rabbitmq/clear_files.sh
+++ rabbitmq/clear_files.sh
@@ -1,3 +1,2 @@
-!/bin/bash
-find /usr/local/django_web/rabbitmq/files/ -type d -mtime +15 -exec rm -rf {} \;
-find /usr/local/blast_dev/web/TmpGifs/ -type f -name *.gif -mtime +15 -exec rm -f {} \;
+#!/bin/bash
+find /usr/local/i5k/media/ -type d -mtime +15 -exec rm -rf {} \;
