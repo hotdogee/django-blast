e08c4515b0874be894a3d9329baec3b0521f5371
jwlin <cs.castman@gmail.com>
2014-08-07 13:05:11 -0400

    virtualenv added

--- i5k/wsgi.py
+++ i5k/wsgi.py
@@ -14,8 +14,24 @@ framework.
 
 """
 import os
+import sys
+import site
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir(os.path.join(PROJECT_ROOT, '/virtualenv/py2.7/lib/python2.7/site-packages'))
+#site.addsitedir('/myprojectenv/local/lib/python2.7/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append(PROJECT_ROOT)
+sys.path.append(os.path.join(PROJECT_ROOT, 'i5k'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "i5k.settings")
+
+# Activate your virtual env
+activate_env=os.path.expanduser(os.path.join(PROJECT_ROOT, 'virtualenv/py2.7/bin/activate_this.py'))
+execfile(activate_env, dict(__file__=activate_env))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
