c41bc3d54a435ea5579a5f333e1393aeffb0ccc0
Han Lin <hotdogee@gmail.com>
2014-08-14 19:13:43 -0400

    fix diacit fasta.

--- util/convert_blast_fasta_id_from_make_new_id_list.py
+++ util/convert_blast_fasta_id_from_make_new_id_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 from os import path
 
-TSV_FILENAME = 'make_new_id_list.tsv'
+#TSV_FILENAME = 'make_new_id_list.tsv'
+TSV_FILENAME = 'make_new_id_list_diacit.tsv'
+#TARGET_DIR = '/app/local/i5k/media/blastdb'
 TARGET_DIR = '/app/local/i5k/media/blastdb'
 
 # read tsv
@@ -12,13 +14,18 @@ with open(TSV_FILENAME, 'rb') as f:
         with open(fasta_filepath, 'rb') as f_in:
             (head, tail) = path.split(fasta_filepath)
             (root, ext) = path.splitext(tail)
-            with open(path.join(TARGET_DIR, root + '_new_ids' + ext), 'wb') as f_out_target:
-                with open(path.join(head, root + '_new_ids' + ext), 'wb') as f_out:
-                    for line in f_in:
-                        if line[0] == '>' and line[:4] != '>gnl':
-                            if db_type == 'Genome Assembly':
-                                line = '>gnl|' + display_name + '|' + short_name + '_' + line[1:]
-                            else:
-                                line = '>gnl|' + display_name + '_' + db_type.lower() + '_' + version + '|' + line[1:]
-                        f_out_target.write(line)
-                        f_out.write(line)
+            #with open(path.join(TARGET_DIR, root + '_new_ids' + ext), 'wb') as f_out_target:
+            #with open(path.join(head, root + '_new_ids' + ext), 'wb') as f_out:
+            with open(root + '_new_ids' + ext, 'wb') as f_out:
+                for line in f_in:
+                    if line[0] == '>' and line[:4] != '>gnl':
+                        seqid = line[1:]
+                        tokens = seqid.split('|', 1)
+                        if len(tokens) == 2:
+                            seqid = tokens[1].replace('|', ' ')
+                        if db_type == 'Genome Assembly':
+                            line = '>gnl|' + display_name + '|' + short_name + '_' + seqid
+                        else:
+                            line = '>gnl|' + display_name + '_' + db_type.lower() + '_' + version + '|' + seqid
+                    #f_out_target.write(line)
+                    f_out.write(line)
