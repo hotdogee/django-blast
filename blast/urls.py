12aaa5e9f1fcdec8449f3231131a78505488ee12
jwlin <cs.castman@gmail.com>
2014-08-14 10:30:21 -0400

    Remove trailing slash of a few urls

--- blast/urls.py
+++ blast/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^$', views.create, name='create'),
     url(r'^iframe$', views.create, {'iframe': True}, name='create'),
     # ex: /blast/5/
-    url(r'^(?P<task_id>[0-9a-fA-F]+)/$', views.retrieve, name='retrieve'),
+    url(r'^(?P<task_id>[0-9a-fA-F]+)$', views.retrieve, name='retrieve'),
     url(r'^read_gff3/(?P<task_id>[0-9a-fA-F]*)/*(?P<dbname>[\w\-\|.]*)/*$', views.read_gff3, name='read_gff3'),
 )
