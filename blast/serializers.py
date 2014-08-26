93578f13956064fe048b17b9370fc82aaad79a27
Han Lin <hotdogee@gmail.com>
2014-08-26 18:32:54 -0400

    REST API can now get blastdb sequences

--- blast/serializers.py
+++ blast/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.pagination import PaginationSerializer
 from .models import Organism, SequenceType, BlastDb, Sequence
 
 class OrganismSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,6 +18,7 @@ class BlastDbSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='blastdb-detail', lookup_field='title')
     organism = serializers.HyperlinkedRelatedField(view_name='organism-detail', lookup_field='short_name')
     type = serializers.HyperlinkedRelatedField(view_name='sequencetype-detail', lookup_field='dataset_type')
+    sequence_set = serializers.HyperlinkedIdentityField(view_name='blastdb-sequence-set', lookup_field='title')
     fasta_file_exists = serializers.Field()
     blast_db_files_exists = serializers.Field()
     sequence_set_exists = serializers.Field()
@@ -24,7 +26,7 @@ class BlastDbSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = BlastDb
-        fields = ('url', 'organism', 'type', 'fasta_file', 'title', 'description', 'is_shown', 'fasta_file_exists', 'blast_db_files_exists', 'sequence_set_exists', 'db_ready', )
+        fields = ('url', 'organism', 'type', 'fasta_file', 'title', 'description', 'is_shown', 'fasta_file_exists', 'blast_db_files_exists', 'sequence_set_exists', 'db_ready', 'sequence_set', )
 
 class SequenceSerializer(serializers.HyperlinkedModelSerializer):
     blast_db = serializers.HyperlinkedRelatedField(view_name='blastdb-detail', lookup_field='title')
@@ -33,3 +35,7 @@ class SequenceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Sequence
         fields = ('blast_db', 'id', 'length', 'seq_start_pos', 'seq_end_pos', 'modified_date', 'fasta_seq', )
+
+class PaginatedSequenceSerializer(PaginationSerializer):
+    class Meta:
+        object_serializer_class = SequenceSerializer
