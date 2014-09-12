1c8fd45443cd7b0edcbe587a1b9b996422ab4f27
Han Lin <hotdogee@gmail.com>
2014-09-12 13:03:40 -0400

    Changed the natural key of BlastDb to title instead of fasta_file path

--- util/build_jbrowsesetting_fixture_from_dataset_list.py
+++ util/build_jbrowsesetting_fixture_from_dataset_list.py
@@ -2,9 +2,9 @@
 import json
 
 TSV_FILENAME = 'i5k_blastdb_list.tsv'
-BLASTDB_URL_ROOT = '/blast/db/'
-DUMP_FILENAME = 'jbrowsesetting-stage.json'
-#DUMP_FILENAME = 'jbrowsesetting.json'
+#BLASTDB_URL_ROOT = '/blast/db/'
+#DUMP_FILENAME = 'jbrowsesetting-stage.json'
+DUMP_FILENAME = 'jbrowsesetting.json'
 
 fixture_list = []
 # read tsv
@@ -15,9 +15,9 @@ with open(TSV_FILENAME, 'rb') as f:
             fixture_item = {
                 'model': 'blast.jbrowsesetting',
                 'fields': {
-                    'url': 'https://apollo.nal.usda.gov/%s/jbrowse/' % short_name,
-                    #'url': 'http://gmod-dev.nal.usda.gov:8080/%s/jbrowse/' % short_name,
-                    'blast_db': [BLASTDB_URL_ROOT + fasta_filename],
+                    #'url': 'https://apollo.nal.usda.gov/%s/jbrowse/' % short_name,
+                    'url': 'http://gmod-dev.nal.usda.gov:8080/%s/jbrowse/' % short_name,
+                    'blast_db': [fasta_filename],
                 }
             }
             fixture_list.append(fixture_item)
