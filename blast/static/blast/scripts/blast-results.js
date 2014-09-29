890dd46aef9f82fb41fced05ff1be01388a17887
Han Lin <hotdogee@gmail.com>
2014-09-29 13:49:18 -0400

    Fixed incorrect height of text search filters on safari

--- blast/static/blast/scripts/blast-results.js
+++ blast/static/blast/scripts/blast-results.js
@@ -660,7 +660,7 @@ $(function () { // document ready
                 }
             });
         } else {
-            var input = $('<div id="' + id + '" class="input-group"><div class="input-group-addon input-sm"><span class="glyphicon glyphicon-search"></div><input type="text" class="form-control input-sm col-search-input ' + title + '" placeholder="' + title + ' filter" /></div></div>').appendTo($(this).empty());
+            var input = $('<div id="' + id + '" class="input-group input-group-sm"><div class="input-group-addon"><span class="glyphicon glyphicon-search"></div><input type="text" class="form-control col-search-input ' + title + '" placeholder="' + title + ' filter" /></div></div>').appendTo($(this).empty());
             $('input', input).on('keyup change', function () {
                 //console.log(colIdx);
                 if (this.value == '') {
