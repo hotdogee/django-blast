5b46723c3a5cb06b9b71a05f6345863c4bdd0679
jwlin <cs.castman@gmail.com>
2015-01-27 17:36:06 -0500

    add user profile, rename some terms, adjust cache timeout for cookies

--- webapollo/admin.py
+++ webapollo/admin.py
@@ -4,11 +4,17 @@ from django.contrib.auth.admin import UserAdmin
 
 class SpeciesAdmin(admin.ModelAdmin):
     list_display = ('name', 'full_name', 'host', 'db_name', 'db_acct', 'db_pwd', 'url',)
+    search_fields = ['name', 'full_name']
+    ordering = ['name']
     actions_on_top = True
     actions_on_bottom = True
 admin.site.register(Species, SpeciesAdmin)
 
 class SpeciesPasswordAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'species__name', 'species__full_name']
+    ordering = ['user__username']
     list_display = ('user', 'species', 'pwd',)
+    actions_on_top = True
+    actions_on_bottom = True
 admin.site.register(SpeciesPassword, SpeciesPasswordAdmin)
 
