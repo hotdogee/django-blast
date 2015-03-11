a12c25cb65bf6c0117da9d89018ceabbfdac6dc5
jwlin <cs.castman@gmail.com>
2015-03-11 17:00:13 -0400

    user permission page for admin

--- webapollo/static/webapollo/scripts/admin-user-permission.js
+++ webapollo/static/webapollo/scripts/admin-user-permission.js
@@ -12,7 +12,7 @@ $(function() { // document ready
     });
 
 
-    $("input[name=owner]").click(function() {
+    $('#table-user-permission tbody').on('click', 'tr input[name=owner]', function () {
         if (this.checked) {
             $(this).parent("td").siblings().find("input").each(function() {
                 $(this).attr("checked", true);
@@ -40,34 +40,49 @@ $(function() { // document ready
 
 
     $('#btn-update').click(function() {
+        $('#btn-update').button('loading')
         var species_permissions = [];
-        $.each(TableTools.fnGetInstance('table-user-permission').fnGetSelectedData(), function(idx, val) {
-            /* example:
-             * ["", "cataqu (Catajapyx aquilonaris)", "<input type="checkbox" name="read" value="1" checked="">", 
-             * "<input type="checkbox" name="write" value="2" checked="">", "<input type="checkbox" name="publist" value="4">", 
-             * "<input type="checkbox" name="admin" value="8">", "<input type="checkbox" name="owner" value="0">"]
-            */
-            var species_name = val[1].split(" ")[0]
-            var permission = 0;
-            var is_owner = false;
-            if ($("#" + species_name).children("td")[2].children[0].checked) permission += 1;
-            if ($("#" + species_name).children("td")[3].children[0].checked) permission += 2;
-            if ($("#" + species_name).children("td")[4].children[0].checked) permission += 4;
-            if ($("#" + species_name).children("td")[5].children[0].checked) permission += 8;
-            if ($("#" + species_name).children("td")[6].children[0].checked) is_owner = true;
-            species_permissions.push({
-                'species_name': species_name,
-                'permission': permission,
-                'is_owner': is_owner,
+        try {
+            $.each(TableTools.fnGetInstance('table-user-permission').fnGetSelectedData(), function(idx, val) {
+                /* example:
+                 * ["", "cataqu (Catajapyx aquilonaris)", "<input type="checkbox" name="read" value="1" checked="">", 
+                 * "<input type="checkbox" name="write" value="2" checked="">", "<input type="checkbox" name="publist" value="4">", 
+                 * "<input type="checkbox" name="admin" value="8">", "<input type="checkbox" name="owner" value="0">"]
+                */
+                var species_name = val[1].split(" ")[0]
+                var permission = 0;
+                var is_owner = false;
+                if ($("#" + species_name).children("td")[2].children[0].checked) permission += 1;
+                if ($("#" + species_name).children("td")[3].children[0].checked) permission += 2;
+                if ($("#" + species_name).children("td")[4].children[0].checked) permission += 4;
+                if ($("#" + species_name).children("td")[5].children[0].checked) permission += 8;
+                if ($("#" + species_name).children("td")[6].children[0].checked) is_owner = true;
+                species_permissions.push({
+                    'species_name': species_name,
+                    'permission': permission,
+                    'is_owner': is_owner,
+                });
             });
-        });
-        console.log(species_permissions);
-        /*$.post(window.location.pathname + "/reject", {'csrfmiddlewaretoken': csrfmiddlewaretoken, 'species_name':v[1] , 'username': v[0], 'comment': comment}, function(data) { 
-            if (data.succeeded) {
-            }
-            else {
-            }
-        }, "json");*/
+            $.post(window.location.pathname, {'species_permissions': JSON.stringify(species_permissions)}, function(data) { 
+                if (data.succeeded) {
+                    $("#alert").css( "color", "#449d44" );
+                    $("#alert").text('Succeeded!').fadeIn().fadeOut(5000);
+                }
+                else {
+                    $("#alert").css( "color", "#d9534f" );
+                    $("#alert").text('Updating failed. Debugging needed..').fadeIn().fadeOut(5000);;
+                }
+                $('#btn-update').button('reset')
+                TableTools.fnGetInstance("table-user-permission").fnSelectNone();
+                $("#select-all").parent("tr").removeClass("selected");
+
+            }, "json");
+        }
+        catch(err) {
+            $("#alert").css( "color", "#d9534f" );
+            $("#alert").text("Some selected rows can not be located. Please make them visible.").fadeIn().fadeOut(5000);
+            $('#btn-update').button('reset')
+        }
     });
 });
 
