c234e048511d0c64bd4bd57cc0026a42b614c523
childers <genetics.guy@gmail.com>
2015-11-06 18:21:25 -0500

    rewrote the forme logic into the view, and adde dlogic to add migration records.

--- migrate_account/urls.py
+++ migrate_account/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add$', views.add, name='add'),
+    url(r'^confirm$', views.confirm, name='confirm'),
 ]
 
--- migrate_account/views.py
+++ migrate_account/views.py
@@ -19,26 +19,40 @@ import traceback
 import stat as Perm
 import os
 import re
-
-from .forms import AddMigrationForm 
+import urllib2
+import cookielib
+from .forms import AddMigrationForm, ConfirmMigrationForm
 
 # Create your views here.
 
 def index(request):
-
-
+    current_user = request.user
     organism_list = sorted([db.id, db.display_name, db.short_name] for db in Organism.objects.all())
-    blastdb_list = sorted([bdb.id, bdb.organism_id] for bdb in BlastDb.objects.all())
-    jbrowse_list = sorted([jdb.id, jdb.url, jdb.blast_db_id] for jdb in JbrowseSetting.objects.all())
-    user_list = sorted([udb.id, udb.organism_id, udb.user_id] for udb in MigrateUserRecord.objects.all())
+    user_list = sorted([udb.id, udb.organism_id, udb.user_id, udb.username, udb.password] for udb in MigrateUserRecord.objects.all().filter(user_id=request.user.id))
+    result_set = []
+
+    for organism in organism_list:
+        blastdb_list = sorted([bdb.id, bdb.organism_id] for bdb in BlastDb.objects.all().filter(organism_id=organism[0]))
+        for blast in blastdb_list:
+            jbrowse_list = sorted([jdb.id, jdb.url, jdb.blast_db_id] for jdb in JbrowseSetting.objects.all().filter(blast_db_id=blast[0]))
+            for jbrowse in jbrowse_list:
+                var = [organism[0], organism[1], organism[2], jbrowse[1], False, None, None]
+                for user in user_list:
+                    if organism[0] == user[1] and user[2] == request.user.id:
+                        var = [organism[0], organism[1], organism[2], jbrowse[1], True, user[3], user[4] ]
+                result_set.append(var)
 
     return render(request,'migrate_account/main.html',{
-            'title': 'Organism Listing', 
-            'user_list': user_list,
-            'blastdb_list': blastdb_list,
-            'jbrowse_list': jbrowse_list,
-            'organism_list': organism_list
-        })
+            'title': 'Organism Listing',
+            'organism_id':organism[0], 
+            'organism_display_name':organism[1], 
+            'organism_short_name':organism[2], 
+            'jbrowse_url':jbrowse[1],
+#            'registered': True,
+#            'username': user[3], 
+#            'password':user[4],
+            'result_set': result_set
+            })
 
 def add(request):
     if request.method == 'POST':
@@ -48,12 +62,32 @@ def add(request):
         if form.is_valid():
             organism_id = form.cleaned_data['organism_id']
             jbrowse_url =  form.cleaned_data['jbrowse_url']
-#            username =  form.cleaned_data['username']
-#            password =  form.cleaned_data['password']
+            organism_display_name = form.cleaned_data['organism_display_name']
+            organism_short_name = form.cleaned_data['organism_short_name']
+            username =  form.cleaned_data['username']
+            password =  form.cleaned_data['password']
 #        return HttpResponseRedirect('/thanks/')
-        return render(request, 'migrate_account/add.html', {'form': form})
+            m = MigrateUserRecord( username = username, password = password, organism_id = organism_id, user_id = request.user.id )
+            m.save()
+            return render(request, 'migrate_account/add.html', {
+                    'organism_id': organism_id,
+                    'organism_short_name': organism_short_name,
+                    'organism_display_name': organism_display_name,
+                    'jbrowse_url': jbrowse_url,
+                    'username': username,
+                    'password': password,
+                    'title': 'Confirm something',
+#                    'result_set': result_set,
+                    'form': form                    
+                    })
     # if a GET (or any other method) we'll create a blank form
-    else:
-        form = AddMigrationForm()
-        
-        return render(request, 'migrate_account/add.html', {'form': form})
+        else:
+            form = AddMigrationForm(request.POST)
+#            form = AddMigrationForm()
+            
+            return render(request, 'migrate_account/add.html', {
+                    'form': form
+                    })
+
+def confirm(request):
+    return()
